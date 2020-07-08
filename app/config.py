1a8ef12609dc0301bce2a2e27f4b4c372d313600
Epic <admin@itzepic.net>
2020-07-08 17:02:04 +0200

    Fix: rewrite config system

---
+++ app/config.py
@@ -0,0 +1,29 @@
+"""
+Created by Epic at 7/8/20
+"""
+from os import environ as env
+import logging
+
+logger = logging.getLogger("api.config")
+
+
+def bool_converter(text: str) -> bool:
+    if isinstance(text, bool):
+        return text
+    values = {
+        "true": True,
+        "false": False
+    }
+
+    value = values.get(text, None)
+    if value is None:
+        raise ValueError(f"Invalid config value. Expected true or false")
+    return value
+
+
+try:
+    DEBUG = bool_converter(env.get("debug", True))
+    DATABASE_URI = env["database_uri"]
+except KeyError as e:
+    logger.critical(f"Missing required config values. Missing setting: {e.args[0]}")
+    raise SystemExit()
--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from app import factory
 from pathlib import Path
 from dotenv import load_dotenv
-from os import environ as env
 
 load_dotenv()
 
 if __name__ == '__main__':
+    from app.config import DEBUG
     # list of all files in the project, so flask knows where to look for changes
     extra_files = [str(p.resolve()) for p in Path('./').glob('**/*')]
-    factory.app.run(extra_files=extra_files, debug=bool(env.get("debug", False)))
+    factory.app.run(extra_files=extra_files, debug=DEBUG)
